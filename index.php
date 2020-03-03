27bc9e9a74bb8ca3bb814fc6c81e006dde47c038
Andrejs Vasiļevskis <andrew@appcloud.lv>
2020-03-03 14:50:19 +0200

    2020 code update ))

--- index.php
+++ index.php
@@ -1,12 +1,35 @@
 <?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
+require __DIR__ . '/vendor/autoload.php';
 
+// Instantiate App
+$app = AppFactory::create();
 
+// Add error middleware
+$app->addErrorMiddleware(true, true, true);
 
-if(!empty($_GET['info']))
-{
-	phpinfo();
-	die();
-}
+// Add routes
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('<a href="/phpinfo/' . INFO_CREDITS . '">phpinfo(INFO_CREDITS);</a>');
+    return $response;
+});
 
-# Привет!
+$app->get('/phpinfo/{what}', function (Request $request, Response $response, $args) {
+    $what = (int)$args['what'];
+    if ($what !== INFO_CREDITS) {
+        $what = INFO_CREDITS;
+    }
+
+    ob_start();
+    phpinfo($what);
+    $info = ob_get_contents();
+    ob_clean();
+
+    $response->getBody()->write($info);
+    return $response;
+});
+
+$app->run();
